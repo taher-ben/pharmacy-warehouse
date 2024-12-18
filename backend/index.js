a660c559dd9beaa2329805c0a160b90863de9550
Hammam Swaid <hmam.swid@gmail.com>
2024-12-18 21:58:40 +0200

    adding authontication to the project

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 app.use(cors());
@@ -20,7 +21,25 @@ db.connect((err) => {
   if (err) {
     console.error("خطأ في الاتصال بقاعدة البيانات:", err);
   } else {
-    console.log("تم الاتصال بقاعدة البيانات");
+    console.log("Database connected successfully");
+    const query = `SELECT * FROM USERS WHERE username = ?`;
+    db.query(query, ["gat warehouse admin"], (err, results) => {
+      if (err) {
+        console.log(err);
+      } else if (results.length === 0) {
+        const createQuery = `INSERT INTO USERS (fullName, username, password) VALUES(?, ?, ?)`;
+        db.query(
+          createQuery,
+          ["مسؤول عام", "gat warehouse admin", "ghat warehouse password"],
+          (err) => {
+            if (err) console.log("حدث خطأ أثناء تسجيل المسؤول", err);
+            else console.log("تم تسجيل المسؤول بنجاح");
+          }
+        );
+      } else {
+        console.log("يوجد مسؤول بالفعل");
+      }
+    });
   }
 });
 
@@ -41,6 +60,58 @@ const updateCurrentStock = (barcode) => {
     }
   });
 };
+
+// create token function
+const createToken = (userId) => {
+  const token = jwt.sign({ id: userId }, "secretKey", { expiresIn: "20h" });
+  return token;
+};
+
+// تسجيل الدخول
+app.post("/api/login", (req, res) => {
+  const { username, password } = req.body;
+  const query = "SELECT * FROM users WHERE username = ? AND password = ?";
+  db.query(query, [username, password], (err, results) => {
+    if (err) {
+      console.error("خطأ في تسجيل الدخول:", err);
+      res.status(500).json({ error: "خطأ في تسجيل الدخول" });
+    } else if (results.length === 0) {
+      res.status(401).json({ error: "اسم المستخدم أو كلمة المرور غير صحيحة" });
+    } else {
+      const token = createToken(results[0].id);
+      res.status(200).json({ message: "تم تسجيل الدخول بنجاح", token });
+    }
+  });
+});
+
+// التحقق من تسجيل الدخول
+const protect = (req, res, next) => {
+  const token = req.headers.authorization.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ error: "لم يتم توفير رمز التحقق" });
+  }
+  jwt.verify(token, "secretKey", (err, decoded) => {
+    if (err) {
+      console.log(err);
+      return res.status(401).json({ error: "رمز التحقق غير صالح" });
+    }
+    userId = decoded.id;
+    const query = "SELECT * FROM users WHERE id = ?";
+    db.query(query, [userId], (err, results) => {
+      if (err) {
+        console.error("خطأ في التحقق من تسجيل الدخول:", err);
+        res.status(500).json({ error: "خطأ في التحقق من تسجيل الدخول" });
+      } else if (results.length === 0) {
+        res.status(401).json({ error: "المستخدم غير موجود" });
+      } else {
+        req.user = results[0];
+        next();
+      }
+    });
+  });
+};
+
+app.use(protect);
 
 // جلب المنتجات من قاعدة البيانات
 app.get("/api/products", (req, res) => {
@@ -439,39 +510,6 @@ app.listen(8081, () => {
   console.log("Server is listening on port 8081");
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // const express = require("express");
 // const mysql = require("mysql2");
 // const cors = require("cors");
@@ -515,7 +553,6 @@ app.listen(8081, () => {
 //       }
 //     });
 //   };
-  
 
 // // Fetch products from the database
 // app.get("/api/products", (req, res) => {
@@ -556,12 +593,12 @@ app.listen(8081, () => {
 // // Add a new product to the database
 // app.post("/api/products/add", (req, res) => {
 //     const { name, category_id, supplier_id, unit_price, quantity_in_stock, minimum_stock_level, expiry_date, barcode,current_stock } = req.body;
-  
+
 //     const query = `
-//       INSERT INTO products (name, category_id, supplier_id, unit_price, quantity_in_stock, minimum_stock_level, expiry_date, barcode,current_stock) 
+//       INSERT INTO products (name, category_id, supplier_id, unit_price, quantity_in_stock, minimum_stock_level, expiry_date, barcode,current_stock)
 //       VALUES (?, ?, ?, ?, ?, ?, ?, ?,?)
 //     `;
-  
+
 //     const values = [
 //       name,
 //       category_id,
@@ -573,7 +610,7 @@ app.listen(8081, () => {
 //       barcode,
 //       quantity_in_stock,
 //     ];
-  
+
 //     db.query(query, values, (err, result) => {
 //       if (err) {
 //         if (err.code === 'ER_DUP_ENTRY') {
@@ -584,18 +621,17 @@ app.listen(8081, () => {
 //         console.error("Error adding product:", err);
 //         return res.status(500).json({ error: "Failed to add product" });
 //       }
-      
+
 //       res.status(201).json({ message: "Product added successfully", productId: result.insertId });
 //     });
 //   });
-  
 
 // // Delete a product
 // app.delete("/api/products/delete/:id", (req, res) => {
 //     const productId = req.params.id;
-  
+
 //     const query = 'DELETE FROM products WHERE product_id = ?';
-  
+
 //     db.query(query, [productId], (err, result) => {
 //       if (err) {
 //         console.error("Error deleting product:", err);
@@ -611,7 +647,7 @@ app.listen(8081, () => {
 //   // Edit Product Endpoint
 // app.put("/api/products/edit/:id", (req, res) => {
 //     const productId = req.params.id;
-//     const { barcode } = req.query; 
+//     const { barcode } = req.query;
 //     const {
 //       name,
 //       category_id,
@@ -621,21 +657,21 @@ app.listen(8081, () => {
 //       minimum_stock_level,
 //       expiry_date,
 //     } = req.body;
-  
+
 //     // Update query
 //     const query = `
-//       UPDATE products 
-//       SET 
-//         name = ?, 
-//         category_id = ?, 
-//         supplier_id = ?, 
-//         unit_price = ?, 
-//         quantity_in_stock = ?, 
-//         minimum_stock_level = ?, 
+//       UPDATE products
+//       SET
+//         name = ?,
+//         category_id = ?,
+//         supplier_id = ?,
+//         unit_price = ?,
+//         quantity_in_stock = ?,
+//         minimum_stock_level = ?,
 //         expiry_date = ?
 //       WHERE product_id = ?
 //     `;
-  
+
 //     const values = [
 //       name,
 //       category_id,
@@ -646,7 +682,7 @@ app.listen(8081, () => {
 //       expiry_date,
 //       productId,
 //     ];
-  
+
 //     // Execute query
 //     db.query(query, values, (err, result) => {
 //       if (err) {
@@ -667,29 +703,28 @@ app.listen(8081, () => {
 //         });
 //       }
 //     });
-//     updateCurrentStock(barcode); 
+//     updateCurrentStock(barcode);
 //   });
-
 
 // // Fetch all categories with their products
 // app.get("/api/category", (req, res) => {
 //     const query = `
-//       SELECT 
-//         c.category_id, 
+//       SELECT
+//         c.category_id,
 //         c.name AS category_name,  -- Alias to make sure it's clear as category name
 //         p.product_id,
 //         p.name AS product_name,   -- Alias to clarify it's the product name
 //         p.current_stock
-//       FROM 
+//       FROM
 //         categories c
-//       LEFT JOIN 
+//       LEFT JOIN
 //         products p
-//       ON 
+//       ON
 //         c.category_id = p.category_id
-//       ORDER BY 
+//       ORDER BY
 //         c.category_id, p.name;
 //     `;
-  
+
 //     db.query(query, (err, results) => {
 //       if (err) {
 //         console.error("Error fetching categories and products:", err);
@@ -706,7 +741,7 @@ app.listen(8081, () => {
 //               products: [],
 //             };
 //           }
-  
+
 //           // Add product if present
 //           if (row.product_id) {
 //             categoriesMap[row.category_id].products.push({
@@ -716,23 +751,22 @@ app.listen(8081, () => {
 //             });
 //           }
 //         });
-  
+
 //         // Convert the map object to an array of categories
 //         const categories = Object.values(categoriesMap);
 //         res.status(200).json(categories);
 //       }
 //     });
 //   });
-  
 
 //   // Add a new category
 // app.post("/api/categories/add", (req, res) => {
 //     const { name } = req.body;
-  
+
 //     if (!name) {
 //       return res.status(400).json({ error: "Category name is required" });
 //     }
-  
+
 //     const query = "INSERT INTO categories (name) VALUES (?)";
 //     db.query(query, [name], (err, result) => {
 //       if (err) {
@@ -759,13 +793,13 @@ app.listen(8081, () => {
 
 //   app.get("/api/scanned_products", (req, res) => {
 //     const query = `
-//       SELECT 
-//         scanned_products.*, 
-//         products.name AS product_name 
+//       SELECT
+//         scanned_products.*,
+//         products.name AS product_name
 //       FROM scanned_products
 //       JOIN products ON scanned_products.barcode = products.barcode
 //     `;
-  
+
 //     db.query(query, (err, results) => {
 //       if (err) {
 //         console.error("Error fetching products:", err);
@@ -775,8 +809,7 @@ app.listen(8081, () => {
 //       }
 //     });
 //   });
-  
-  
+
 //   app.get("/api/products/:barcode", (req, res) => {
 //     const { barcode } = req.params;
 //     db.query("SELECT name, expiry_date FROM products WHERE barcode = ?", [barcode], (err, results) => {
@@ -812,61 +845,59 @@ app.listen(8081, () => {
 //   });
 // });
 
-  
-  
 //   app.put("/api/scanned_products/:id", (req, res) => {
 //     const { id } = req.params;
 //     const { quantity, barcode } = req.body;
-  
+
 //     const query = "UPDATE scanned_products SET quantity = ? WHERE id = ?";
 //     db.query(query, [quantity, id], (err, result) => {
 //       if (err) {
 //         console.error("Error updating scanned product:", err);
 //         return res.status(500).json({ error: "Failed to update the scanned product" });
 //       }
-  
+
 //       if (result.affectedRows === 0) {
 //         return res.status(404).json({ error: "Scanned product not found" });
 //       }
-//       updateCurrentStock(barcode); 
+//       updateCurrentStock(barcode);
 //       res.json({ message: "Product updated successfully" });
 //     });
 //   });
 
 //   app.delete("/api/scanned_products/:id", (req, res) => {
 //     const { id } = req.params;
-//     const { barcode } = req.query; 
-  
+//     const { barcode } = req.query;
+
 //     const query = "DELETE FROM scanned_products WHERE id = ?";
 //     db.query(query, [id], (err, result) => {
 //       if (err) {
 //         console.error("Error deleting scanned product:", err);
 //         return res.status(500).json({ error: "Failed to delete the scanned product" });
 //       }
-  
+
 //       if (result.affectedRows === 0) {
 //         return res.status(404).json({ error: "Scanned product not found" });
 //       }
-//       updateCurrentStock(barcode); 
-  
+//       updateCurrentStock(barcode);
+
 //       res.json({ message: "Product deleted successfully" });
 //     });
 //   });
-  
+
 //   // Function to add a new scanned product (new scan)
 // app.post("/api/scanned_products", (req, res) => {
 //     const { barcode, quantity, scanned_at } = req.body; // Get product details from the request body
-  
+
 //     // SQL query to insert the new scanned product into the database
 //     const query = "INSERT INTO scanned_products (barcode, quantity, scanned_at) VALUES (?, ?, ?)";
-  
+
 //     db.query(query, [barcode, quantity, scanned_at], (err, result) => {
 //       if (err) {
 //         console.error("Error adding product:", err);
 //         return res.status(500).json({ message: "Failed to add product." });
 //       }
-//       updateCurrentStock(barcode); 
-  
+//       updateCurrentStock(barcode);
+
 //       // Successfully added
 //       return res.status(201).json({ message: "Product added successfully.", productId: result.insertId });
 //     });
@@ -888,7 +919,6 @@ app.listen(8081, () => {
 //     });
 // });
 
-  
 //   app.get("/api/suppliers", (req, res) => {
 //     db.query("SELECT * FROM suppliers", (err, results) => {
 //       if (err) {
@@ -898,7 +928,7 @@ app.listen(8081, () => {
 //       res.json(results);
 //     });
 //   });
-  
+
 //   app.post("/api/suppliers", (req, res) => {
 //     const { name, contact, address } = req.body;
 //     db.query("INSERT INTO suppliers (name, contact_details, address) VALUES (?, ?, ?)", [name, contact, address], (err, result) => {
@@ -925,8 +955,7 @@ app.listen(8081, () => {
 //       }
 //     );
 //   });
-  
-  
+
 //   app.delete("/api/suppliers/:id", (req, res) => {
 //     const { id } = req.params;
 //     db.query("DELETE FROM suppliers WHERE supplier_id = ?", [id], (err, result) => {
@@ -937,7 +966,6 @@ app.listen(8081, () => {
 //       res.json({ message: "Supplier deleted successfully" });
 //     });
 //   });
-  
 
 // // Server Listening
 // app.listen(8081, () => {
